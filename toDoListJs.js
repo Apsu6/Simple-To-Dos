d2f58df4ad9b1c038033057a965356ea84697700
Apsu <83819490+Apsu6@users.noreply.github.com>
2021-05-11 19:10:31 +0300

    Add files via upload

--- toDoListJs.js
+++ toDoListJs.js
@@ -4,7 +4,11 @@ let vue = new Vue ({
     data:{
         newTask: '',
         existingTask:[
-            {text :'Cooking'}
+
+        ],
+        latertask:[
+
+
         ]
 
 
@@ -18,8 +22,23 @@ let vue = new Vue ({
         },
         deletetask(i){
             this.existingTask.splice(i,1)
-        }
+            console.log(i)
+        },
+        later(i){
+            this.latertask.push(this.existingTask[i])
+            console.log(this.existingTask)
+            this.existingTask.splice(i,1)
 
+        },
+        deletelatertask(i){
+            this.latertask.splice(i,1)
+            console.log(i)},
+        Redo(i){
+            this.existingTask.push(this.latertask[i])
+            console.log(this.latertask)
+            this.latertask.splice(i,1)
+
+        }
 
 
     }
